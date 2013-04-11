ea83dc64d03be1d1ea6ed3ae8962003d3d04a4c0
jiangyong <460429192@qq.com>
2013-04-11 16:30:00 +0800

    入口文件

---
+++ index.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>业务管理系统 - 系统登录</title>
+        <link rel="stylesheet" type="text/css" href="public/js/extjs/resources/css/ext-all.css" />
+        <script type="text/javascript" src="public/js/extjs/ext-all.js"></script>
+        <script type="text/javascript" src="public/js/extjs/locale/ext-lang-zh_CN.js"></script>
+        <link rel="stylesheet" type="text/css" href="public/css/login.css" />
+        <link rel="stylesheet" type="text/css" href="public/css/ext_icon.css" />
+        <script type="text/javascript" src="views/app/view/login.js"></script>
+    </head>
+    <body>
+        <div id="hello-tabs" style="display:none;">
+            <img border="0" width="450" height="70" src="public/images/login_banner.png" />
+        </div>
+    </body>
+</html>
