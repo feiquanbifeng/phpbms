17ea049a32c6e1a3f093874371996e2e491254ab
chenzj <460629192@qq.com>
2013-04-11 22:02:12 +0800

    修改入口文件

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
-﻿<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>业务管理系统 - 系统登录</title>
-        <link rel="stylesheet" type="text/css" href="public/js/extjs/resources/css/ext-all.css" />
-        <script type="text/javascript" src="public/js/extjs/ext-all.js"></script>
-        <script type="text/javascript" src="public/js/extjs/locale/ext-lang-zh_CN.js"></script>
-        <link rel="stylesheet" type="text/css" href="public/css/login.css" />
-        <link rel="stylesheet" type="text/css" href="public/css/ext_icon.css" />
-        <script type="text/javascript" src="views/app/view/login.js"></script>
-    </head>
-    <body>
-        <div id="hello-tabs" style="display:none;">
-            <img border="0" width="450" height="70" src="public/images/login_banner.png" />
-        </div>
-    </body>
-</html>
+﻿<?php
+// +----------------------------------------------------------------------
+// | PHPBMS [ PHP BMS ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: JY
+// +----------------------------------------------------------------------
+
+// PHPBMS 入口文件
+// 记录开始运行时间
+$GLOBALS['_beginTime'] = microtime(TRUE);
+
+// 记录内存初始使用
+define('MEMORY_LIMIT_ON', function_exists('memory_get_usage'));
+if (MEMORY_LIMIT_ON) {
+    $GLOBALS['_startUseMems'] = memory_get_usage();
+}
+
+// 系统目录定义
+defined('APP_PATH') 	or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
+defined('RUNTIME_PATH') or define('RUNTIME_PATH', realpath(APP_PATH).'/runtime/');
+defined('APP_DEBUG') 	or define('APP_DEBUG', false); // 是否调试模式
+
+$runtime = defined('MODE_NAME')?'~'.strtolower(MODE_NAME).'_runtime.php':'~runtime.php';
+defined('RUNTIME_FILE') or define('RUNTIME_FILE', RUNTIME_PATH.$runtime);
+if(!APP_DEBUG && is_file(RUNTIME_FILE)) {
+    require RUNTIME_FILE;
+} else {
+    // 加载运行时文件
+    require APP_PATH.'common/runtime.php';
+}
